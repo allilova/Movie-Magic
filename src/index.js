ccde8d8dd3af56d53f8b9baaa0f79426a68bddb6
allilova <80039522+allilova@users.noreply.github.com>
2024-10-08 13:47:15 +0300

    setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("App work");
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works');
+});
+
+app.listen(5000, () => console.log('Server on http://localhost:5000...'));
