57099cc275821b25bd7755ab79c763fc521b8d8a
allilova <80039522+allilova@users.noreply.github.com>
2024-10-08 15:05:17 +0300

    render homepage

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }))
 app.set('view engine', 'hbs');
-
+app.set('views', './src/views');
+app.use(express.static('static'));
 app.get('/', (req, res) => {
-    res.send('Just works');
+    res.render('home.hbs');
 });
 
 app.listen(5000, () => console.log('Server on http://localhost:5000...'));
