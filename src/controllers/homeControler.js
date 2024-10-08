316fa62b8b0fd253f82b57d46a59ea86a5dc8038
allilova <80039522+allilova@users.noreply.github.com>
2024-10-08 15:40:11 +0300

    add about page

--- src/controllers/homeControler.js
+++ src/controllers/homeControler.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('home.hbs');
 });
+router.get('/about', (req, res)=>{
+    res.render('home/about');
+})
 
 export default router;
