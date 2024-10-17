315e226e19b26a1e9b4da03f84d0e0c413017e4d
allilova <80039522+allilova@users.noreply.github.com>
2024-10-17 11:43:46 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import homeControler from './controllers/homeControler.js'
+import homeControler from './controllers/homeControler.js';
+import movieController from './controllers/movieController.js';
 
 const router = Router();
 
 
 router.use(homeControler);
+router.use('/movies', movieController);
+router.all('*', (req, res) => {
+    res.render('404');
+})
 
 export default router;
