fe2b942c0f42a59163ae5e02de9ae65b78739ed3
allilova <80039522+allilova@users.noreply.github.com>
2024-10-17 16:50:54 +0300

    connect to mongoosedb

---
+++ src/config/mongooseInit.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+export default async function mongooseInit() {
+    try{
+        const url = process.env.DB_URL || 'mongodb://localhost:27017';
+        await connect(url, {dbName: 'magic-movies'});
+
+        console.log('Successfully connected to DB!');
+    }catch(err){
+        console.log('Cannot connesct to DB!' + err.message);
+    }
+}
