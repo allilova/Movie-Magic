859606f5bf6f6fda5439d909096b6dd03ce5422c
allilova <80039522+allilova@users.noreply.github.com>
2024-10-24 18:43:56 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import homeControler from './controllers/homeControler.js';
 import movieController from './controllers/movieController.js';
-
+import castController from './controllers/castController.js';
 const router = Router();
 
 
 router.use(homeControler);
 router.use('/movies', movieController);
+router.use('/casts', castController)
 router.all('*', (req, res) => {
     res.render('404');
 })
