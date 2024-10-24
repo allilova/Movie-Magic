b93ebf295a4f66f718fec32db52b90e74ab55839
allilova <80039522+allilova@users.noreply.github.com>
2024-10-24 17:16:41 +0300

    Update mongooseInit.js

--- src/config/mongooseInit.js
+++ src/config/mongooseInit.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export default async function mongooseInit() {
     try{
-        const url = process.env.DB_URL || 'mongodb://localhost:27017';
+        const url = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
         await connect(url, {dbName: 'magic-movies'});
 
         console.log('Successfully connected to DB!');
