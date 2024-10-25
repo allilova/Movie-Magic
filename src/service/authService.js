8a5649ef5c60f6ebf9536af4f4c51ab4fc841580
allilova <80039522+allilova@users.noreply.github.com>
2024-10-25 12:34:35 +0300

    update service

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,29 +1,29 @@
 import bcrypt from 'bcrypt';
 import jwt from '../lib/jwt.js';
 
-import User from '../models/User.js';
+import User from "../models/User.js";
 import { JWT_SECRET } from '../config/constants.js';
 
 const register = async (email, password, rePassword) => {
-    const userCount = await User.countDocuments({email});
+    const userCount = await User.countDocuments({ email });
 
-    if(userCount > 0){
+    if (userCount > 0) {
         throw new Error('User already exists');
     }
 
-    return User.create({ email, password, rePassword});
+    return User.create({ email, password, rePassword });
 };
 
-const login = async (email, password) =>{
-    const user = await User.findOne({email});
+const login = async (email, password) => {
+    const user = await User.findOne({ email });
 
-    if(!user){
+    if (!user) {
         throw new Error('User does not exist!');
     }
 
     const isValid = await bcrypt.compare(password, user.password);
 
-    if(!isValid){
+    if (!isValid) {
         throw new Error('Password does not match');
     }
 
@@ -32,7 +32,7 @@ const login = async (email, password) =>{
         email,
     };
 
-    const token = await jwt.sign(payload, JWT_SECRET, { expiresIn: '2h'});
+    const token = await jwt.sign(payload, JWT_SECRET, { expiresIn: '2h' });
 
     return token;
 }
--- src/service/castService.js
+++ src/service/castService.js
@@ -2,12 +2,11 @@ import Cast from "../models/Cast.js";
 
 const getAll = () => Cast.find();
 
-const getAllWithout = (casts) =>{
+const getAllWithout = (casts) => {
     const castIds = casts.map(cast => cast.cast._id);
 
     return Cast.find().nin('_id', castIds);
 }
-
 
 const create = (cast) => Cast.create(cast);
 
