31d63468bb716612b55b5c5e3816598092135dd4
allilova <80039522+allilova@users.noreply.github.com>
2024-10-25 10:02:43 +0300

    update router

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,17 @@ import { Router } from "express";
 import homeControler from './controllers/homeControler.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castController.js';
+import authController from './controllers/authController.js';
+import { isAuth } from "./middlewares/authMiddleware.js";
+
 const router = Router();
 
 
 router.use(homeControler);
 router.use('/movies', movieController);
-router.use('/casts', castController)
+router.use('/casts', isAuth, castController);
+router.use('/auth', authController);
+
 router.all('*', (req, res) => {
     res.render('404');
 })
