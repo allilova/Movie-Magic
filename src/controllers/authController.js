31ef556d573621ac43849daf517a455017e6d415
allilova <80039522+allilova@users.noreply.github.com>
2024-10-25 09:29:27 +0300

    Create authController.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import validator from "validator";
+
+import authService from '../service/authService.js';
+import { getErrorMessage} from '../utils/errorUtils.js';
+import { emit } from "nodemon";
+
+const router = Router();
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) =>{
+    const { email, password, rePassword } = req.body;
+
+    try{
+        await authService.register(email, password, rePassword);
+    }catch(err){
+        return res.render('auth/register', { error: getErrorMessage(err), email});
+        }
+
+        const token = await authService.login(email, password);
+        res.cookie('auth', token, { httpOnly: true});
+        res.redirect('/');
+    });
+
+    router.get('/login', (req, res) =>{
+        res.render('auth/login');
+    });
+
+    router.post('/login', async (req, res) =>{
+        const{ email, password } = req.body;
+
+        const token = await authService.login(email, password);
+
+        res.cookie('auth', token, { httpOnly: true });
+        res.redirect('/');
+    });
+
+    router.get('/logout', (req, res) => {
+        res.clearCookie('auth');
+
+        res.redirect('/');
+    });
+
+    export default router; 
