f8f2e47396099fbdae23cd8a0b796b0d58187a39
allilova <80039522+allilova@users.noreply.github.com>
2024-10-25 09:04:13 +0300

    Update movieService.js

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -18,17 +18,23 @@ const getAll = (filter = {}) => {
     return moviesQuery;
 };
 
-const create = (movie) => Movie.create(movie);
+const create = (movie) => Movie.create({ ...movie, owner: ownerId });
 
 const getOne = (movieId) => Movie.findById(movieId).populate('casts.cast'); 
 
 const attach = (movieId, castId, character) => {
     return Movie.findByIdAndUpdate(movieId, { $push: { casts: {cast: castId, character}}});
-}
+};
+
+const remove = (movieId) => Movie.findByIdAndDelete(movieId);
+
+const edit = (movieId, data) => Movie.findByIdAndUpdate(movieId, data);
 
 export default {
     getAll,
     create,
     getOne,
-    attach
+    attach,
+    remove,
+    edit,
 }
