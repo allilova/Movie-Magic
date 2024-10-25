a2de93c42d3c4fa0f0982ec2e5aee59356449ef8
allilova <80039522+allilova@users.noreply.github.com>
2024-10-25 12:35:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import express from "express";
-import handlebarsInit from "./config/handlebarsInit.js";
-import expressInit from "./config/expressInit.js";
+import express from 'express';
+import 'dotenv/config' 
+
 import routes from './routes.js';
-import mongooseInit from "./config/mongooseInit.js";
+import handlebarsInit from './config/handlebarsInit.js';
+import expressInit from './config/expressInit.js';
+import mongooseInit from './config/mongooseInit.js';
 
 const app = express();
+
 mongooseInit();
 handlebarsInit(app);
 expressInit(app);
 
 app.use(routes);
 
-
-app.listen(5000, () => console.log('Server on http://localhost:5000...'));
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
